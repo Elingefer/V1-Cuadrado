c7d433bac44a8951696ab8664aedc4b70cffa6a5
El Inge Fer <Feralvarex02@gmail.com>
2023-09-19 04:22:16 +0200

    AREA CUADRADO

--- V1 Cuadrado/V1 Cuadrado.cpp
+++ V1 Cuadrado/V1 Cuadrado.cpp
@@ -1,19 +1,14 @@
-// V1 Cuadrado.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    // Definir la longitud del lado del cuadrado en centímetros
+    double lado = 5.0;
+
+    // Calcular el área del cuadrado (lado * lado)
+    double area = lado * lado;
+
+    // Mostrar el resultado
+    std::cout << "El área del cuadrado con un lado de " << lado << " cm es: " << area << " cm^2" << std::endl;
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
