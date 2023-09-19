faa41f659282585d1491233988d4920ec68bd563
El Fer <143768867+Elingefer@users.noreply.github.com>
2023-09-19 04:35:57 +0200

    Add files via upload

---
+++ V2 Rectangulo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Definir la longitud de los lados del rectángulo en centímetros
+    double largo = 23.0;
+    double ancho = 23.0;
+
+    // Calcular el área del rectángulo (largo * ancho)
+    double area = largo * ancho;
+
+    // Mostrar el resultado
+    std::cout << "El área del rectángulo con largo de " << largo << " cm y ancho de " << ancho << " cm es: " << area << " cm^2" << std::endl;
+
+    return 0;
+}
